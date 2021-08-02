220fd6c763638d7acf469ee46d09f3e381e8b684
gyubum <sgb8170@naver.com>
2021-08-02 14:24:26 +0900

    Add LoginActivity.kt Add activity_login.xml

--- app/src/main/java/org/techtown/wishmatching/MainActivity.kt
+++ app/src/main/java/org/techtown/wishmatching/MainActivity.kt
@@ -3,12 +3,21 @@ package org.techtown.wishmatching
 // commit test kjh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
+import com.google.android.gms.common.SignInButton
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  //test kim
 
+
+
+
         //test1 xngsoo
     }
+
+
 }
