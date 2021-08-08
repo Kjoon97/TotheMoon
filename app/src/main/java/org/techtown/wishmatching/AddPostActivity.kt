13019f86966aca6f50cd89145271dec812082efa
gyubum <sgb8170@naver.com>
2021-08-08 14:49:49 +0900

    게시물 등록 기능 구현

--- app/src/main/java/org/techtown/wishmatching/AddPostActivity.kt
+++ app/src/main/java/org/techtown/wishmatching/AddPostActivity.kt
@@ -1,17 +1,48 @@
 package org.techtown.wishmatching
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_post.*
+import kotlinx.android.synthetic.main.activity_profile.*
+import org.techtown.wishmatching.Database.PostDTO
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPostActivity : AppCompatActivity() {
+
+    var PICK_IMAGE_FROM_ALBUM=0  //request code
+    var storage : FirebaseStorage? = null
+    var photoUri: Uri? = null // 이미지 URI 담을 수 있음
+    var auth: FirebaseAuth? = null   // 유저의 정보를 가져오기 위함
+    var firestore : FirebaseFirestore? = null   // 데이터베이스를 사용할 수 있도록
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
 
+        storage = FirebaseStorage.getInstance() //스토리지 초기화
+        auth = FirebaseAuth.getInstance()            //초기화
+        firestore = FirebaseFirestore.getInstance()  //초기화
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        // 게시글에 올릴 사진 선택 버튼
+        btn_post_image.setOnClickListener{
+            var photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type ="image/*"
+            startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBUM)
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,8 +53,48 @@ class AddPostActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.profilemenu_test-> startActivity(Intent(this,ProfileActivity::class.java))
+            R.id.action_addpost -> {
+                contentUpload()
+                //액티비티 실행
+                startActivity(Intent(this,MainActivity::class.java))
+
+            }
         }
+
         return super.onOptionsItemSelected(item)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode ==PICK_IMAGE_FROM_ALBUM)
+            if(resultCode == Activity.RESULT_OK){  //사진을 선택했을 때 이미지의 경로가 이쪽으로 넘어옴
+                photoUri = data?.data    //경로담기
+                btn_post_image.setImageURI(photoUri)   //선택한 이미지로 변경
+            }else{  //취소버튼 눌렀을 때 작동하는 부분
+                finish()  //취소했을 때는 액티비티 그냥 취소
+            }
+    }
+    fun contentUpload(){ // 파이어베이스 로드
+
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())//파일이름 입력해주는 코드 - 이름이 중복 설정되지않도록 파일명을 날짜로
+        var imageFileName = "IMAGE_"+timestamp+"_.png"
+
+        var storageRef =storage?.reference?.child("Post")?.child(imageFileName)
+
+        //callback 방식
+        //파일 업로드 //데이터베이스를 입력해주는코드
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            storageRef.downloadUrl.addOnSuccessListener { uri->
+                firestore!!.collection("post")
+                    .whereEqualTo("uid", auth?.uid)
+                    .get()
+                    .addOnSuccessListener { documents->
+                        firestore?.collection("post")?.document()
+                            ?.set(PostDTO("${uri.toString()}", "${auth?.uid}", editText_title.text.toString(), editText_content.text.toString(), "아직미정"))
+
+                    }
+                setResult(Activity.RESULT_OK)
+                finish()
+            }
+        } //파일업로드 성공 시 이미지 주소를 받아옴 ,받아오자마자 데이터 모델을 만듦듦
     }
 }
---
+++ app/src/main/java/org/techtown/wishmatching/Database/PostDTO.kt
@@ -0,0 +1,8 @@
+package org.techtown.wishmatching.Database
+
+data class PostDTO(   var imageUrl :String?=null,
+                      var uid :String? =null,
+                      var title: String?=null,
+                      var content : String? = null,
+                      var category : String? = null ){
+}
