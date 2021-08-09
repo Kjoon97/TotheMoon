a1bf0a49c8c8c27023631d2cb792e0480ae9fa64
Chaeduk <kkj2551@naver.com>
2021-08-09 12:27:18 +0900

    myProfile 업데이트

--- app/src/main/java/org/techtown/wishmatching/MyPageFragment.kt
+++ app/src/main/java/org/techtown/wishmatching/MyPageFragment.kt
@@ -1,14 +1,55 @@
 package org.techtown.wishmatching
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.fragment_my_page.*
 
 class MyPageFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null   // 데이터베이스를 사용할 수 있도록
+    var storage : FirebaseStorage? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        firestore = FirebaseFirestore.getInstance()  //초기화
+        storage = FirebaseStorage.getInstance() //스토리지 초기화
+
+        firestore!!.collection("user")
+            .whereEqualTo("uid", Authentication.auth.currentUser!!.uid).limit(1)
+            .get()
+            .addOnSuccessListener { documents->
+                for(document in documents){
+                    mypage_location.text = document.data["area"].toString()
+                    mypage_nickname.text = document.data["nickname"].toString()
+                    val image = storage!!.getReferenceFromUrl(document.data["imageUrl"].toString())
+                    displayImageRef(image, img_myPage_profileImg)
+
+                }
+            }
+
+
+
+    }
+
+    private fun displayImageRef(imageRef: StorageReference?, view: ImageView) {
+        imageRef?.getBytes(Long.MAX_VALUE)?.addOnSuccessListener {
+            val bmp = BitmapFactory.decodeByteArray(it, 0, it.size)
+            view.setImageBitmap(bmp)
+        }?.addOnFailureListener {
+            // Failed to download the image
+        }
     }
 }
